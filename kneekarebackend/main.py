a5557a52ba154b2f9aa7545271ae9f6f67bd8acb
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-10-28 12:58:33 -0400

    adding models

--- kneekarebackend/main.py
+++ kneekarebackend/main.py
@@ -2,6 +2,29 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+from sqlmodel import SQLModel, create_engine, Field, Session, select
+from typing import Annotated, List, Optional
+from datetime import datetime
+
+DATABASE_URL = "sqlite:///instance/kneekare.db"
+engine = create_engine(DATABASE_URL, echo=True)
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    password: str
+    email: str
+    
+class KneeData(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    timestamp: datetime = Field(default_factory=datetime.now)
+    angle: float
+    rotation: float
+    
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 app = FastAPI()
 
 # CORS
@@ -20,9 +43,27 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+@app.post("/users", response_model=User)
+async def create_user(user: User):
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+    
+@app.get("/users", response_model=List[User])
+async def read_users():
+    with Session(engine) as session:
+        users = session.exec(select(User)).all()
+        return users
 
 def start():
     uvicorn.run("kneekarebackend.main:app", host="localhost", port=8000, reload=True)
