cf8dd39e9875e8bcef3979d1db95f113037407fa
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-10-28 12:40:33 -0400

    init

---
+++ kneekarebackend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+# CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+def start():
+    uvicorn.run("kneekarebackend.main:app", host="localhost", port=8000, reload=True)
