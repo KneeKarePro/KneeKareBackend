ccff32821082b91c71c12e9ac178ac27e5c5658c
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-11-05 12:37:21 -0500

    adding interactions between kneedata and user space using some functions from sqlmodel orm

--- kneekarebackend/routers/users.py
+++ kneekarebackend/routers/users.py
@@ -4,8 +4,9 @@ from sqlmodel import Session, select, SQLModel, Field
 from typing import List, Optional
 
 # Local Imports
-from kneekarebackend.models.user import User # Import the User model
-from kneekarebackend.database import engine # Import the database engine
+from kneekarebackend.models.user import User  # Import the User
+from kneekarebackend.models.kneedata import KneeData  # Import the KneeData
+from kneekarebackend.database import engine  # Import the database engine
 
 
 user_router: APIRouter = APIRouter(
@@ -13,7 +14,8 @@ user_router: APIRouter = APIRouter(
     tags=["users"],
     responses={404: {"description": "Not found"}},
 )
-   
+
+
 @user_router.get("/", response_model=List[User])
 async def read_users():
     """
@@ -28,7 +30,8 @@ async def read_users():
     with Session(engine) as session:
         users = session.exec(select(User)).all()
         return users
-      
+
+
 @user_router.post("/", response_model=User)
 async def create_user(user: User):
     """
@@ -45,3 +48,61 @@ async def create_user(user: User):
         session.commit()
         session.refresh(user)
         return user
+
+
+@user_router.get("/{user_id}", response_model=User)
+async def read_user(user_id: int):
+    """
+    Get a user by their id
+
+    - Args
+        user_id (int): The id of the user to be retrieved
+
+    - Returns
+        User: The user with the specified id
+    """
+    with Session(engine) as session:
+        user = session.get(User, user_id)
+        return user
+
+
+# Get all user knee data
+@user_router.get("/{user_id}/knee-data", response_model=List[KneeData])
+async def read_user_knee_data(user_id: int):
+    """
+    Get all knee data for a user
+
+    - Args
+        user_id (int): The id of the user to get knee data for
+
+    - Returns
+        List[KneeData]: A list of all knee data for the user
+    """
+    with Session(engine) as session:
+        user = session.get(User, user_id)
+        knee_data = session.exec(
+            select(KneeData).filter(KneeData.user_id == user_id)
+        ).all()
+        return knee_data
+
+
+# Create knee data for a user
+@user_router.post("/{user_id}/knee-data", response_model=KneeData)
+async def create_user_knee_data(user_id: int, knee_data: KneeData):
+    """
+    Create knee data for a user
+
+    - Args
+        user_id (int): The id of the user to create knee data for
+        knee_data (KneeData): The knee data to be created
+
+    - Returns
+        KneeData: The knee data that was created
+    """
+    with Session(engine) as session:
+        user = session.get(User, user_id)
+        knee_data.user_id = user_id
+        session.add(knee_data)
+        session.commit()
+        session.refresh(knee_data)
+        return knee_data
